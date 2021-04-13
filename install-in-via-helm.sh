c2c30646710e1e7e30657d993dc319deb6921308
Igo go <igogo@ddd.com>
2021-04-13 16:56:24 +0300

    fix helm install

--- install-in-via-helm.sh
+++ install-in-via-helm.sh
@@ -1,7 +1,6 @@
 kubectl create namespace ingress-nginx
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
-helm install ingress-nginx ingress-nginx/ingress-nginx
 helm install nginx-ingress ingress-nginx/ingress-nginx \
     --namespace ingress-nginx \
     --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
